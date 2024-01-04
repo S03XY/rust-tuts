aab1c5fc119b071d2e142ce1f8d8092df1564cad
crpto genesis <prishankifficial@gmail.com>
2024-01-04 21:43:57 +0530

    ownership explained

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,26 @@
-mod  tut2;
+mod tut2;
 mod tut3;
 
 // how do we define a function in rust ?
 // fn <function_name>(arg1:type,arg2:type,arg3:type...) -> type {
-    //  return <object_of_return_type>
-    // }
+//  return <object_of_return_type>
+// }
 // use pub keyword when this function is used outside a module of from the current current file
 
-pub fn script_saga_function_one(_arg1:u32,_arg2:&String,_arg3:bool) -> (u32,bool) {
-    // _args => remove compiler warning but binds the variables 
+pub fn script_saga_function_one(_arg1: u32, _arg2: &String, _arg3: bool) -> (u32, bool) {
+    // _args => remove compiler warning but binds the variables
     // _ => use this if you want to completely ignore the varibles and its useful for defining function signatures or in pattern matching
-
 
     // code here...
 
-
-
-    // return  (23,true); OR if you dont want to use "return" term then use 
-    (23,true) 
+    // return  (23,true); OR if you dont want to use "return" term then use
+    (23, true)
 }
 
-
-
-
-
-
-
-
-
-
-fn  main() {
-
+fn main() {
     // tut2::function_return_a_result();
     tut3::variable_in_rust()
 
     // let arg2 = String::from("Shashank");
     // script_saga_function_one(1, &arg2, true);
-    
 }
-
-
-
-
--- src/tut3.rs
+++ src/tut3.rs
@@ -3,11 +3,11 @@
 pub fn variable_in_rust() {
     // A variable can be used to store data or a pointer to a specific memory location in Rust.
     // Variables can be defined and initialized using the 'let' or 'const' keyword.
-    // Variables are immutable by default and can be made mutable by adding the 'mut' keyword. 
+    // Variables are immutable by default and can be made mutable by adding the 'mut' keyword.
     // (There is a smart pointer type that does not require explicit initialization with the 'mut' keyword, which will be discussed later.)
 
     /* Important:
-        Rust behaves differently for data stored in stack memory and data stored in heap memory, 
+        Rust behaves differently for data stored in stack memory and data stored in heap memory,
         as specified by ownership and borrow rules, which we will explore later.
     */
 
@@ -36,4 +36,38 @@ pub fn variable_in_rust() {
     arg_mutable = true;
 
     // Will discuss complex types in some other snapshot.
+}
+
+// Understanding Rust ownership in a simpler way.
+
+/*
+    When a program is executed, it has two types of memory: stack and heap.
+    Working with stack memory is faster compared to heap memory because variables
+    are stored in memory at compile time in an ordered manner, whereas data in heap memory
+    is stored in an unordered manner. Therefore, the allocator needs to do some work to find a space
+    to store a particular data and keep a book tracking it. That's why pushing data into
+    stack memory is faster than allocating data in the heap.
+
+    Ownership rules:
+    1. Each value in Rust has an owner.
+    2. There can only be one owner at a time.
+    3. When the owner goes out of scope, the value will be dropped.
+
+    The data whose size can't be known at compile time is called dynamic data and is stored
+    in heap memory, while the data whose size can be known at compile time is stored in stack memory.
+    So when you reassign a variable to another variable, Rust first checks if the data is sized or unsized.
+    If it is sized, Rust simply copies and pushes it onto the stack. If it's unsized data,
+    Rust transfers the ownership to the new variable, and the other variable becomes invalid.
+
+    Ownership can be transferred to other variables, functions, as return values from functions, threads, closures,
+    and if you don't want to transfer ownership, you borrow. We'll look into that in the next segment.
+*/
+
+pub fn understand_ownership() {
+    let a: u32 = 4;
+    let b = a; // Its size is known at compile time, and hence it is copied and stored in the stack
+
+    let unsized_data = String::from("ScriptSaga");
+    let get_ownership = unsized_data; // This is unsized data, and then the ownership is transferred to a new variable, and unsized data is not valid anymore
+    
 }
